01fa76caaa13f4d9189c481a37be50069d2e97fa
Sasha <buyalex@ukr.net>
2023-09-11 13:58:56 +0200

    cookieParser add

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}))
 
+var cookieParser = require("cookie-parser");
+app.use(cookieParser())
+app.use(express.json())
+
 app.use(route)
 app.listen(3000, ()=> {
     console.log('Running on port 3000');
