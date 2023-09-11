5f3bc7502cb4e12830392f64093b06b020df819c
Sasha <buyalex@ukr.net>
2023-09-11 13:32:09 +0200

    hello mongo

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+//Hello
 mongoose.connect('mongodb+srv://aysnr0204:aysnrakca01@cluster0.1p0zvyl.mongodb.net/')
 .then(()=> console.log('Connected DB'))
 .catch((err)=> console.log('You have a problem about connecting DB', err))
