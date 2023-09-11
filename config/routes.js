ee6b4b2dd2588763e03393fa81830dbffac9a256
Sasha <buyalex@ukr.net>
2023-09-11 13:49:13 +0200

    add login signup

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const placeController = require('../controller/placeController')
-const reviewController = require('../controller/reviewController')
 const route = express.Router();
+const userController = require('../controller/userController')
+const auth = require('../middleware/auth')
+
 
 route.get('/', placeController.mainPage)
 route.get('/places', placeController.getPlaces)
@@ -11,7 +13,13 @@ route.post('/places', placeController.addNewPlace)
 route.get('/places/:id/edit', placeController.editPage)
 route.post('/places/:id/edit', placeController.updatePlace)
 route.post('/places/:id/delete', placeController.deletePlace)
-route.post('/places/:id/reviews', reviewController.saveReview)
-route.post('/places/:id/reviews/:reviewid', reviewController.deleteReview)
+
+route.get('/', auth.userAuth, userController.mainPage)
+route.get('/login', auth.loginAuth,userController.logIn)
+route.post('/signup-user', userController.createUser)
+route.post('/login-user', userController.logInUser)
+route.get('/logout-user', userController.logOut)
+
+
  
 module.exports = route;
